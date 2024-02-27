4004a299b3a0422a26db4d7c1d38a8e4863da0d2
Zediwan <Jeremy@qualit.ch>
2024-02-27 18:27:51 +0100

    Updated pre_processing to not override original "big" fiels

--- src/Pre-Processing.R
+++ src/Pre-Processing.R
@@ -1,10 +1,12 @@
 library(tidyverse)
+library(quanteda)
 library(dplyr)
 
-filename = "feminism.csv"
-link = str_c("data/reddit_", filename)
+subreddit = "feminism"
+link = str_c("data/big_reddit_", subreddit)
+filename = str_c (link, ".csv")
 
-df = read.csv(link)
+df = read.csv(filename)
 df = df %>% select(-X)
 
 # Remove deleted columns
@@ -12,4 +14,15 @@ df <- df %>% mutate(Text = gsub("\\[deleted\\], ", "", Text))
 df = df %>% filter(Text != "")
 df = df %>% filter(Text != ", ")
 
+# Remove short convs
+threshold <- 20  # Min length of conv
+corpus <- corpus(df$Text)
+text_lengths <- nchar(corpus)
+df <- df[text_lengths >= threshold, ] # Filter out rows with text length below the threshold
+
+# Shrink in size to not have to large files for github
+df = df %>% head(30000)
+
+link = str_c("data/reddit_", subreddit)
+filename = str_c (link, ".csv")
 write.csv(df, filename)
