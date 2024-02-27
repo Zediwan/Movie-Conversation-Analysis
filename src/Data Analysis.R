71db1e2b3fea7c2287cef841eb98c95e5c5cbbd3
Zediwan <Jeremy@qualit.ch>
2024-02-27 17:36:36 +0100

    Removed short conversations

--- src/Data Analysis.R
+++ src/Data Analysis.R
@@ -1,10 +1,16 @@
 library(tidyverse)
+library(quanteda)
 
+df.movie_conversation = read.csv("data/movie_conversation.csv")
 
-df.movie_conversation = read.csv("movie_conversation.csv")
-df.reddit = read.csv("reddit_post_progressive")
+threshold <- 50  # Min length of conv
 
-number_of_movies = df %>% select(Movie.Title) %>% unique() %>% nrow()
+number_of_movies = df.movie_conversation %>% select(Movie.Title) %>% unique() %>% nrow()
 
-df.releases_per_year = df %>% group_by(Release.Year) %>% summarise(num_releases = length(unique(Movie.Title)))
-df.releases_per_year %>% ggplot(aes(x=Release.Year, y = num_releases)) + geom()                                             
+corpus <- corpus(df.movie_conversation$Text)
+text_lengths <- nchar(corpus)
+df.movie_conversation <- df.movie_conversation[text_lengths >= threshold, ] # Filter out rows with text length below the threshold
+#write.csv(df.movie_conversation, "data/movie_conversation.csv")
+
+df.movie_conversation.releases_per_year = df.movie_conversation %>% group_by(Release.Year) %>% summarise(num_releases = length(unique(Movie.Title)))
+df.movie_conversation.releases_per_year %>% ggplot(aes(x=Release.Year, y = num_releases)) + geom_line()                                           
