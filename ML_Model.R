2f0e959ea6fb831214ef0048a2fa5a2a68ce0457
andrin.M <andrin.mueller@students.unibe.ch>
2024-02-28 14:29:32 +0100

    security commit

--- ML_Model.R
+++ ML_Model.R
@@ -8,38 +8,50 @@ library(caret)
 
 
 #Data preperation for tolerance = tolerant
-progressive_tweets <- read.csv("progressive-tweet-sentiment.csv")
-
-tolerant_tweets <- progressive_tweets %>% 
+progressive_tweet_sentiment <- read.csv("data/progressive-tweet-sentiment.csv")
+progressive_tweet_sentiment <- progressive_tweet_sentiment %>% 
   filter(target == "Legalization of Abortion" | target == "Feminist Movement"
          | target == "Atheism") %>%
   mutate(tolerance = "tolerant", text = tweet) %>% 
   select(tolerance, text) 
 
 
+reddit_posts_progressive_small <- read.csv("data/reddit_posts_progressive_small.csv")
+reddit_posts_progressive_small <- reddit_posts_progressive_small %>% 
+  mutate(text = Text, tolerance = "tolerant") %>% 
+  select(text, tolerance)
+
 
 #Data Preperation for tolerance = neutral (slur _language.csv indlucds neutral topics)
-slur_reddits<- read.csv("slur_language.csv")
+slur_languagexlsx<- read.csv("data/slur_languagexlsx.csv")
 
-neutral_reddits <- slur_reddits %>% 
+neutral_reddits <- slur_languagexlsx %>% 
   filter(annotation_Primary == "Neutral" & meta_text != "" ) %>% 
   mutate(tolerance = " neutral",text = meta_text) %>% 
   select(tolerance, text)
 
 #Data Preperation tolerant = intoleran
-intolerant_reddits <- slur_reddits %>% 
+intolerant_reddits <- slur_languagexlsx %>% 
   filter(annotation_Primary == "Slur" |
            annotation_Primary =="PersonDirectedAbuse" |
            annotation_Primary == "IdentityDirectedAbuse"|
            annotation_Primary == "AffiliationDirectedAbuse" &
            meta_text != "" ) %>% 
-  mutate(tolerance = " intolerant",text = meta_text) %>% 
+  mutate(tolerance = "intolerant",text = meta_text) %>% 
+  select(tolerance, text)
+
+hate_speech <- read.csv("data/hatespeach.csv")
+hate_speech <- hate_speech %>% 
+  mutate(tolerance = "intolerant", text = tweet) %>% 
   select(tolerance, text)
 
 
 #Glueing the three categories togheter
-tolerance <- rbind(tolerant_tweets, neutral_reddits, intolerant_reddits)
-######################################################
+tolerance <- rbind(progressive_tweet_sentiment, neutral_reddits,
+                   intolerant_reddits, reddit_posts_progressive_small,
+                   hate_speech)
+
+###########################################################################
 
 #ML-Model
 corpus <- corpus(tolerance)
@@ -74,8 +86,6 @@ correct <- dfm_correct$tolerance
 tab_class <- table(correct, prediction)
 tab_class
 confusionMatrix(tab_class, mode = "prec_recall")
-
-
 
 
 
