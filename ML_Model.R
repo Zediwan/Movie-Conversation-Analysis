6a6cb5156c824693d107cc726d6c49f825df07e2
Zediwan <Jeremy@qualit.ch>
2024-03-03 16:32:23 +0100

    Result plotting now implemented

--- ML_Model.R
+++ ML_Model.R
@@ -1,13 +1,13 @@
-library(tidyverse)
-library(quanteda.textmodels)
-library(quanteda.textstats)
-library(MLmetrics)
-library(htmltools)
-library(quanteda)
-library(quanteda.corpora)
-library(caret)
-library(plotly)
-library(Rcpp)
+  library(tidyverse)
+  library(quanteda.textmodels)
+  library(quanteda.textstats)
+  library(MLmetrics)
+  library(htmltools)
+  library(quanteda)
+  library(quanteda.corpora)
+  library(caret)
+  library(plotly)
+  library(Rcpp)
 
 #Data preperation for tolerance = tolerant
 progressive_tweet_sentiment <- read.csv("data/progressive-tweet-sentiment.csv")
@@ -164,4 +164,74 @@ predictions_unlabeled_movie <- predict(review_model, newdata = dfm_unlabeled_mov
 
 unlabeled_movie$predicted_label <- predictions_unlabeled_movie
 
+# Tidying up the df
+unlabeled_movie$Release.Year = str_replace(unlabeled_movie$Release.Year, "/.*", "")
+unlabeled_movie %>% select(Release.Year) %>% unique()
+unlabeled_movie <- unlabeled_movie %>% mutate(Genre = str_replace_all(Genre, "\\[|\\]", ""))
+# Function to transform genre strings into vectors
+string_to_vector <- function(genre_string) {
+  # Remove leading and trailing single quotes
+  genre_string <- gsub("^'|'$", "", genre_string)
+  # Split the string by comma followed by a space
+  unlist(strsplit(genre_string, "', '"))
+}
+# Apply the function to the Genre column
+unlabeled_movie <- unlabeled_movie %>%
+  mutate(Genre = lapply(Genre, string_to_vector))
+# Unnest the Genre column
+unlabeled_movie <- unlabeled_movie %>%
+  tidyr::unnest(Genre)
 
+# Analysis
+movies_summary <- unlabeled_movie %>%
+  group_by(Movie.ID, Movie.Title, Release.Year, Genre) %>%
+  summarise(num_convs = n(),
+            num_tolerant = sum(predicted_label == "tolerant"),
+            num_neutral = sum(predicted_label == "neutral"),
+            num_intolerant = sum(predicted_label == "intolerant"),
+            score = ((num_tolerant + 1) / (num_intolerant + 1)) / num_convs) %>%
+  filter(num_convs >= 20)
+  
+# Extract the decade from the year
+movies_summary$Decade <- as.numeric(substr(movies_summary$Release.Year, 1, 3)) * 10
+
+# Group movies into 10-year intervals
+movies_summary$Decade_Group <- paste(movies_summary$Decade, "-", movies_summary$Decade + 9)
+
+year_summary = movies_summary %>% group_by(Release.Year) %>% 
+  summarise(num_movies = n(), avg_score = sum(score, na.rm = T) / num_movies)
+
+decade_summary = movies_summary %>% group_by(Decade_Group) %>% 
+  summarise(num_movies = n(), avg_score = sum(score, na.rm = T) / num_movies)
+
+year_summary %>% ggplot(aes(x = Release.Year, y = avg_score)) +
+  geom_line(group = 1) +
+  theme(axis.text.x = element_text(angle = 90, vjust = 0.5, hjust=1))
+decade_summary %>% ggplot(aes(x = Decade_Group, y = avg_score)) +
+  geom_line(group = 1) +
+  theme(axis.text.x = element_text(angle = 90, vjust = 0.5, hjust=1))
+
+genre_summary <- movies_summary %>%
+  group_by(Release.Year, Genre) %>%
+  summarise(
+    num_movies = n(),
+    avg_score = sum(score, na.rm = TRUE) / num_movies
+  )
+
+genre_totals <- genre_summary %>%
+  group_by(Release.Year) %>%
+  summarise(total_movies = sum(num_movies))
+
+genre_summary <- genre_summary %>%
+  left_join(genre_totals, by = "Release.Year") %>%
+  mutate(perc_of_total = (num_movies / total_movies) * 100)
+
+genres = c("drama", "crime")
+genres <- unique(genre_summary$Genre)
+
+genre_summary %>%
+  ggplot(aes(x = Release.Year, y = perc_of_total, color = Genre)) +
+  geom_line() +
+  theme(axis.text.x = element_text(angle = 90, vjust = 0.5, hjust = 1)) +
+  labs(title = "Percentage of Drama and Crime Movies Released Each Year",
+       x = "Release Year", y = "Percentage of Total Movies")
